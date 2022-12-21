a6b6e711f0424c6c0d54a87dc336b1b128132f47
Debora Durek <debora.durek@gmail.com>
2022-12-20 18:32:56 -0700

    Fundational concepts

--- intro_to_fp/basic_functional_examples.js
+++ intro_to_fp/basic_functional_examples.js
@@ -1,18 +1,29 @@
 // -----------------------------------------------------------------
 // Exercise 1
 // Directions: Write a pure function that prints "good afternoon" if
-//       its afternoon and "good morning" any other time of the day.
+//       it's afternoon and "good morning" any other time of the day.
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
+const dayGreeting = () => {
+  const currentHour = new Date().getHours();
 
-
-
+  if (currentHour >= 12 && currentHour < 17) {
+    return 'Good Afternoon!';
+  } else {
+    return 'Good Morning!';
+  }
+};
 
 // -----------------------------------------------------------------
 // Exercise 2
-// Directions: Write a pure function that takes in a number and  
-//       returns an array of items counting down from that number to 
+// Directions: Write a pure function that takes in a number and
+//       returns an array of items counting down from that number to
 //       zero.
 // -----------------------------------------------------------------
-
-
+const countNumberDown = (number) => {
+  let countArray = [];
+  for (let i = 0; i <= number; i++) {
+    countArray.push(number - i);
+  }
+  return countArray;
+};
