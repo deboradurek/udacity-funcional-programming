3bf339d6fd0ce86ed2d269f72c68a1e179a020ad
Debora Durek <debora.durek@gmail.com>
2022-12-20 18:33:15 -0700

    The Map Method

--- functional_js_syntax/array_adv_map.js
+++ functional_js_syntax/array_adv_map.js
@@ -1,20 +1,34 @@
 // 1. TODO
-["1", "2", "3"].map(parseInt)
-
+['1', '2', '3'].map(parseInt);
 
 // ----------------------------------------------------------
 // REAL LIFE EXERCISES
 // ----------------------------------------------------------
-// One common thing we use map for in real life is to reformat objects to have a shape that is 
-// better for our purposes. For instance, we only care about the value and first three characters 
+// One common thing we use map for in real life is to reformat objects to have a shape that is
+// better for our purposes. For instance, we only care about the value and first three characters
 // of the id for the races below. Use map to grab those values and create a new array with them.
 
-var index = [
-    {key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon'},
-    {key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari'},
-    {key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon'},
-    {key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa'},
-]
+const index = [
+  { key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon' },
+  { key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari' },
+  { key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon' },
+  { key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa' },
+];
+
+const newIndex = index.map((item) => {
+  return {
+    id: item.id.slice(0, 3),
+    value: item.value,
+  };
+});
+
+console.log(newIndex);
+// [
+//   { id: '123', value: 'Klingon' },
+//   { id: '293', value: 'Minbari' },
+//   { id: '283', value: 'Cylon' },
+//   { id: 'n98', value: 'Jawa' }
+// ]
 
 // Expected Output:
 // [
--- functional_js_syntax/array_map.js
+++ functional_js_syntax/array_map.js
@@ -1,43 +1,63 @@
 // 1. Write a map function to reverse this array:
-const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// your code
+const reverseArray = (array) =>
+  array.map((_, index) => array[array.length - 1 - index]);
 
-// expected output: Array [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+console.log(reverseArray(start));
+// [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
 
 // ----------------------------------------------------------
 // 2. Write a map function to print the Job: Name:
-const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], ["Jayne", "Public Relations"]]
+const shipMates = [
+  ['Mal', 'Captain'],
+  ['Wash', 'Pilot'],
+  ['Zoey', '1st Mate'],
+  ['Jayne', 'Public Relations'],
+];
 
-// your code
+const JobName = shipMates.map((item) => `${item[1]}: ${item[0]}`);
 
-// expected output: Array ["Captain: Mal", etc...]
+console.log(JobName);
+// [ 'Captain: Mal','Pilot: Wash','1st Mate: Zoey','Public Relations: Jayne' ]
 
 // ----------------------------------------------------------
 // 3. Write a map function that prints the name: even|odd
-const awayTeam = ["Picard", "Riker", "Troy", "Data"]
+const awayTeam = ['Picard', 'Riker', 'Troy', 'Data'];
 
 // your code
+const evenOdd = awayTeam.map((name, index) => {
+  if (index % 2 === 0) {
+    return `${name}: even`;
+  } else {
+    return `${name}: odd`;
+  }
+});
 
-// expected output: Array: ["Picard: even", "Riker: odd", etc...]
+console.log(evenOdd);
+// [ 'Picard: even', 'Riker: odd', 'Troy: even', 'Data: odd' ]
 
 // ----------------------------------------------------------
 // 4. Create a multidimensional array of each item and its index in the original Array.
 
-const sci_fi_shows = ['Manedlorian', 'Enterprise', 'Firefly', 'Battlestar Galactica']
+const sci_fi_shows = [
+  'Manedlorian',
+  'Enterprise',
+  'Firefly',
+  'Battlestar Galactica',
+];
 
-// your code
+const sciFiWithIndex = sci_fi_shows.map((show, index) => [show, index]);
 
-// expected output: Array [['Manedlorian', 0], ['Enterprise', 1], ['Firefly', 2], ['Battlestar Galactica', 3]]
+console.log(sciFiWithIndex);
+// [ [ 'Manedlorian', 0 ],[ 'Enterprise', 1 ],[ 'Firefly', 2 ],[ 'Battlestar Galactica', 3 ] ]
 
 // ----------------------------------------------------------
 // 5. For each item in this array, create a multidimensional array containing the entire original array.
 
-const numbers = [1, 2, 3, 4]
+const numbers = [1, 2, 3, 4];
 
-// your code
+const allNumbers = numbers.map(() => numbers);
 
-// expected output: Array [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]
-
-// Having trouble with creating a 2D array? 
-// Take a closer look at the last two arguments of map, one of them might be able to help
+console.log(allNumbers);
+// [ [ 1, 2, 3, 4 ], [ 1, 2, 3, 4 ], [ 1, 2, 3, 4 ], [ 1, 2, 3, 4 ] ]
