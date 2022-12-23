7246f2e686baffacb54cd2af4504261cff201b7f
Debora Durek <debora.durek@gmail.com>
2022-12-22 18:47:46 -0700

    The object freeze method

--- functional_js_syntax/object_freeze.js
+++ functional_js_syntax/object_freeze.js
@@ -6,16 +6,16 @@
 // Node runs in strict mode automatically, as do many online code editors, so if you are running these examples in the terminal or online you probably won't need this
 // If you want to learn more about strict mode, here is a good article: https://www.geeksforgeeks.org/strict-mode-javascript/
 
-"use strict";
+'use strict';
 
 const currentShow = {
-    title: 'Dr. Who',
-    seasons: 11,
-    currentSeason: 4
-}
+  title: 'Dr. Who',
+  seasons: 11,
+  currentSeason: 4,
+};
 
 // as a const, we can do this:
-currentShow.currentSeason = 5
+currentShow.currentSeason = 5;
 
 // but if we freeze the object
 Object.freeze(currentShow);
@@ -23,8 +23,20 @@ Object.freeze(currentShow);
 currentShow.currentSeason = 5;
 // Throws an error and current_season remains unchanged
 
-
 // OBJECT FREEZE EXERCISE
 
 // ----------------------------------------------------------
 // Directions: Create your own object and freeze it!
+const myDog = {
+  name: 'Olsen George',
+  nickname: 'Pitchuculo',
+  age: 4,
+};
+
+myDog.name = 'Zeus';
+
+Object.freeze(myDog);
+
+myDog.name = 'OtherName';
+
+console.log(myDog);
