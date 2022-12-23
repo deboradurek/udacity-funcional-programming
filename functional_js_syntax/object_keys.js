aebc178df79e6d54f3988f7980931923d4caa12a
Debora Durek <debora.durek@gmail.com>
2022-12-22 19:00:19 -0700

    The Object.keys method

--- functional_js_syntax/object_keys.js
+++ functional_js_syntax/object_keys.js
@@ -1,18 +1,22 @@
 // Use Object.keys with the map method to print "This character has a(n) [property-name]" for each property
 
 const character = {
-    id: '12mn18udcbv9823',
-    name: 'Chewbacca',
-    race: 'Wookie',
-    planet: 'Kashyyyk',
-    job: 'First Mate'
+  id: '12mn18udcbv9823',
+  name: 'Chewbacca',
+  race: 'Wookie',
+  planet: 'Kashyyyk',
+  job: 'First Mate',
 };
 
-console.log(Object.keys(character));
-// Expected Output: 
-// This character has a(n) id.
-// This character has a(n) name.
-// This character has a(n) race.
-// This character has a(n) planet.
-// This character has a(n) job.
-// [ 'This character has a(n) id',  'This character has a(n) name',  'This character has a(n) race', 'This character has a(n) planet', 'This character has a(n) job' ]
+const characterArray = Object.keys(character);
+
+const result = characterArray.map((item) => `This character has a(n) ${item}.`);
+
+console.log(result);
+// [
+//   'This character has a(n) id.',
+//   'This character has a(n) name.',
+//   'This character has a(n) race.',
+//   'This character has a(n) planet.',
+//   'This character has a(n) job.'
+// ]
