26a2d31c46ebc7f8ce784d949d74fa666d9b247d
Debora Durek <debora.durek@gmail.com>
2022-12-22 19:15:43 -0700

    The object.assign method

--- functional_js_syntax/object_assign.js
+++ functional_js_syntax/object_assign.js
@@ -1,44 +1,42 @@
 // Merge another character into this state. Keep at least one value and change at least one value
 
 let state = {
-    name: 'Wash',
-    ship: {
-        name: 'Serenity',
-        class: 'Firefly'
+  name: 'Wash',
+  ship: {
+    name: 'Serenity',
+    class: 'Firefly',
+  },
+  role: 'Pilot',
+  favoriteThing: {
+    item: 'Toy',
+    details: {
+      type: 'Toy Tyrannosaurus Rex',
     },
-    role: 'Pilot',
-    favoriteThing: {
-        item: "Toy",
-        details: {
-            type: 'Toy Tyrannosaurus Rex'
-        }
-    }
-}
+  },
+};
 
 const incomingState = {
-    name: 'Mal',
-    role: 'Captain',
-    favoriteThing: {
-        item: "Not complicated"
-    },
-    history: ["Browncoat sergeant"]
-}
+  name: 'Mal',
+  role: 'Captain',
+  favoriteThing: {
+    item: 'Not complicated',
+  },
+  history: ['Browncoat sergeant'],
+};
 
 state = Object.assign(state, incomingState);
-// Object.assign(target, source)
 
-// Resulting object
-// state = { 
-//     name: "Mal", 
-//     ship: { 
-//         name: "Serenity", 
-//         class: "Firefly" 
-//     }, 
-//     role: "Captain", 
-//     favoriteThing: { 
-//         item: "Not complicated" 
-//     }, 
-//     history: ["Browncoat sergeant"] 
-// } 
+const anotherState = {
+  name: 'Sterling',
+  series: 'Amazon Prime',
+};
 
-// Your Code here
+state = Object.assign(state, anotherState);
+// {
+//  name: 'Sterling',
+//  ship: { name: 'Serenity', class: 'Firefly' },
+//  role: 'Captain',
+//  favoriteThing: { item: 'Not complicated' },
+//  history: [ 'Browncoat sergeant' ],
+//  series: 'Amazon Prime'
+// }
