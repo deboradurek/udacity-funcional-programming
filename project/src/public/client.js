9622438c332263ed9d80cdbf23633679dacc3853
Debora Durek <debora.durek@gmail.com>
2023-01-22 21:00:23 -0700

    Implement mars dashboard

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,227 @@
+Object.defineProperty(Array.prototype, 'mapComp', {
+  value: function (callbackFn) {
+    return this.map(callbackFn).join('');
+  },
+});
+
 let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+  rovers: undefined,
+};
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+  store = Object.assign(store, newState);
+  render(root);
+};
 
-const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+const render = async (root) => {
+  root.innerHTML = App();
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+const App = () => {
+  return `
+    <header class="pb-3 mb-4 border-bottom">
+      <span class="fs-4">Mars Dashboard</span>
+    </header>
+    <main>
+      <section>
+        ${RoversTabs()}
+      </section>
+    </main>
+    <footer></footer>
+    `;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
-
+  render(root, store);
+});
 // ------------------------------------------------------  COMPONENTS
+const RoverImage = ({ photo, index, total }) => {
+  return `
+    <img src="${photo}" height="300" class="rounded-start w-100">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>${index + 1} from ${total}</h5>
+    </div>
+  `;
+};
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+const RoverContent = ({ rover }) => {
+  return `
+    <div class="pt-4">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            ${Carousel({
+              id: `carousel-${rover.id}`,
+              items: rover.photos.map((photo, index) =>
+                RoverImage({ photo, index, total: rover.photos.length })
+              ),
+            })}
+          </div>
+          <div class="col-md-8">
+            <div class="card-body h-100 d-flex flex-column">
+              <div class="flex-grow-1">
+                <h5 class="card-title">${rover.name} rover</h5>
+                <p class="card-text fw-semibold text-primary-emphasis">
+                  ${rover.status}
+                </p>
+              </div>
+              <span class="card-text mt-2">
+                <small class="text-muted">Launch at ${rover.launch_date}</small>
+              </span>
+              <span class="card-text">
+                <small class="text-muted">Landing at ${rover.landing_date}</small>
+              </span>
+              <span class="card-text">
+                <small class="text-muted">Photos from ${rover.max_date}</small>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+const RoversPlaceholder = () => {
+  return `
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item">
+        <p class="nav-link active placeholder-glow" style="width: 100px">
+          <span class="placeholder col-12"></span>
+        </p>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div class="pt-4">
+        <div class="card mb-3" aria-hidden="true">
+          <div class="row g-0">
+            <div class="col-md-4">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#868e96"></rect>
+            </svg>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body h-100 d-flex flex-column">
+                <div class="flex-grow-1">
+                  <h5 class="card-title placeholder-glow">
+                    <span class="placeholder col-4"></span>
+                  </h5>
+                  <p class="card-text placeholder-glow">
+                    <span class="placeholder col-2"></span>
+                  </p>
+                </div>
+                <span class="card-text mt-2 placeholder-glow">
+                  <span class="placeholder col-4"></span>
+                </span>
+                <span class="card-text placeholder-glow">
+                  <span class="placeholder col-4"></span>
+                </span>
+                <span class="card-text placeholder-glow">
+                  <span class="placeholder col-4"></span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+const RoversTabs = () => {
+  const { rovers } = store;
+  if (!rovers) {
+    getRovers();
+    return RoversPlaceholder();
+  }
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+  const tabs = rovers.map((rover, index) => ({
+    id: rover.name,
+    title: rover.name,
+    content: RoverContent({ rover }),
+    isSelected: index === 0,
+  }));
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+  return Tabs({ id: 'rovers', tabs });
+};
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+// ------------------------------------------------------  REUSABLE COMPONENTS
+const CarouselItem = ({ content, isActive }) => {
+  return `
+    <div class="carousel-item ${isActive ? 'active' : ''}">
+      ${content}
+    </div>
+  `;
+};
+
+const Carousel = ({ id, items }) => {
+  return `
+    <div id="${id}" class="carousel slide">
+      <div class="carousel-inner">
+        ${items.mapComp((content, index) => CarouselItem({ content, isActive: index === 0 }))}
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#${id}" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#${id}" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  `;
+};
+
+const TabItem = ({ id, title, isSelected }) => {
+  return `
+    <li class="nav-item" role="presentation">
+      <button
+        class="nav-link ${isSelected ? 'active' : ''}"
+        id="${id}-tab"
+        data-bs-toggle="tab"
+        data-bs-target="#${id}-tab-pane"
+        type="button" role="tab"
+        aria-controls="${id}-tab-pane"
+        aria-selected="${isSelected ? 'true' : 'false'}">
+        <h6>${title}</h6>
+      </button>
+    </li>
+    `;
+};
+
+const TabContent = ({ id, content, isSelected }) => {
+  return `
+    <div
+      class="tab-pane fade ${isSelected ? 'show active' : ''}"
+      id="${id}-tab-pane" role="tabpanel"
+      aria-labelledby="home-tab"
+      tabindex="0">
+      ${content}
+    </div>
+  `;
+};
+
+const Tabs = ({ id, tabs }) => {
+  return `
+    <ul class="nav nav-tabs" id="${id}-tab" role="tablist">
+      ${tabs.mapComp((tab) => TabItem(tab))}
+    </ul>
+    <div class="tab-content" id="${id}-tab-content">
+      ${tabs.mapComp((tab) => TabContent(tab))}
+    </div>
+  `;
+};
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
-}
+const getRovers = () => {
+  fetch(`http://localhost:3000/rovers`)
+    .then((res) => res.json())
+    .then((rovers) => updateStore(store, { rovers }));
+};
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,28 +1,63 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
+const Immutable = require('immutable');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+const request = async (path, params) => {
+  const urlParams = new URLSearchParams({
+    api_key: process.env.API_KEY,
+    ...params,
+  });
+  return fetch(`https://api.nasa.gov/${path}?${urlParams}`).then((res) => res.json());
+};
+
+const getRovers = () => request(`mars-photos/api/v1/rovers`);
+
+const getRoverInfo = (roverName, date) =>
+  request(`mars-photos/api/v1/rovers/${roverName.toLowerCase()}/photos`, {
+    earth_date: date,
+  });
 
 // your API calls
+app.get('/rovers', async (_req, res) => {
+  try {
+    const { rovers } = await getRovers();
+    const photos = (
+      await Promise.all(rovers.map((rouver) => getRoverInfo(rouver.name, rouver.max_date)))
+    ).map((result) => ({
+      photos: result.photos.map((photo) => photo.img_src),
+    }));
+
+    const result = Immutable.List(rovers)
+      .zipWith((a, b) => {
+        return { ...a, ...b };
+      }, photos)
+      .toJS();
+
+    res.send(result);
+  } catch (err) {
+    console.log('error:', err);
+  }
+});
 
 // example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+app.get('/apod', async (_req, res) => {
+  try {
+    const image = await request('planetary/apod');
+    res.send({ image });
+  } catch (err) {
+    console.log('error:', err);
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
